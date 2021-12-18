b4a9933d03fc7ccd65a98dcbe3ca76833c51348c
rksdev <mailto.dev.rakesh@gmail.com>
2021-12-18 22:49:57 +0530

    final commit

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const githubAxios = axios.create({
   baseURL: GITHUB_URL,
-  headers: {
-    Authorization: `token ${GITHUB_TOKEN}`,
-  },
+  //   headers: {
+  //     Authorization: `token ${GITHUB_TOKEN}`,
+  //   },
 });
 
 //Get Search Results
